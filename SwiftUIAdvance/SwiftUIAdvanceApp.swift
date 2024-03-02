0cbb7239abc7bbc662378ffd0c55b00f98f527c4
Heri Susanto <herisusanto.app@gmail.com>
2024-03-02 16:54:53 +0700

    Initial Commit

---
+++ SwiftUIAdvance/SwiftUIAdvanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvanceApp.swift
+//  SwiftUIAdvance
+//
+//  Created by loratech on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
